c1d245ea6d422d375f61bbda73668691a9b79c1f
vovka <9141778236@mail.ru>
2022-06-29 23:37:57 +1000

    2.1.1 попытки с динамическимим классами времени выполнения. Ушел на работу

--- classes/avvDNSBase.ps1
+++ classes/avvDNSBase.ps1
@@ -1,10 +1,11 @@
+#using module '.\avvDNSProvider.ps1'
+#. .\avvDNSProvider.ps1
+
 class avvDNSBase{
     [string]hidden $provider='_none_'
     [hashtable]$params=[ordered]@{}
     [array]$classes=@()
-
     [string]$pwd=(Split-Path $psCommandPath -Parent)
-
 
     avvDNSBase([string]$provider)
     {
@@ -25,68 +26,215 @@ class avvDNSBase{
 
     [Array]GetDomains()
     {
-        return $this.GetData('domainsList')
+        return $this.MethodDispath('domainsList')
     }
 
     <#
-        Работа с провайдером.$this
-
+        Диспетчеризация методов  класса для работы с провайдером
         Вход:
-            [string]$command
+            [string]$Method
                 'domainsList'
                 'recordsList,
                 'getRecord,
                 'newRecord,
                 'changeRecord,
-        Возврат:
-            [hashtable]{'status'=[int]; 'result'=[array]@()}
-                status  = 0 - нет ошибок
-                        = 1 - нет провайдера
-                        = 2 - нет такого класса, файла с классом, ошибка создания объекта
-                        = 3 -
-                        = 4 -
-                        = 5 -
-    #>
-    [hashtable] hidden GetData([string]$command)
+        Выход:
+            [Hashtable]
+                [int] status
+                                = 0 - нет ошибок
+                                = 1 - нет провайдера
+                                = 2 - нет файлов с реализацией класса
+                                = 3 - нет такого класса
+                                = 4 - ошибка создания экземпляра класса
+                                = 4 - ошибка вызова метода для проверки реализован ли метод в классе
+                                = 5 -
+                [hashtable] compute - вычисленные данные, переменные во время выполнения функции
+                [array]     result  - массив записей с сервера DNS
+                [Hashtable] raw     - raw данные с сервера, http ответ на REST запрос
+                [String]    msg     - сообщение об ошибке, если status <> 0
+                [String]    msgException    - системное сообщение об Exception, если status <> 0
+     #>
+    [Array] MethodDispath([String]$Method)
     {
-        $errros=@(
-            "OK"
-            "Нет провайдера {0}"
-            "Нет класса {0}"
+        $errors=@(
+        "OK"
+        "Нет реализации API для провайдера {0}"
+        "Ошибка открытия файла реализации API {0} для класса {1}"
+        "Нет класса {0}"
+        "Ошибка создания экземпляра класса {0}"
+        "Ошибка вызова метода {0} класса {1}"
+        "Метод {0} не поддерживается в данном классе реализации {1}"
         )
         $result = @{
             status=0
+            compute=@{}
             msg=""
             msgException=""
             result=@()
+            raw=@{}
         }
-        #$result.cp=$psCommandPath #--------------------------------------------------------------
-        $classNameProvider='avvDNS' + $this.ToTitleCase($this.provider)
-        if ($this.provider.ToLower() -ne '_none_')
-        {
-            try
-            {
-                Import-Module ".\$($classNameProvider).ps1"
-                #Import-Module "./avvDNSSelectel.ps1"
-                #$classProvider=New-Object -TypeName $classNameProvider -ArgumentList $this.provider;
-                $classProvider=New-Object -TypeName $classNameProvider
+        $arrObj=@()
 
-                ############# delete
-                $result.qwerty=$classProvider
-            }
-            catch
-            {
-                $result.status=2 # нет такого класса, файла с классом, ошибка создания объекта
-                $result.msgException=$PSItem.Exception.Message
-            }
-        }
-        else
+        $ClassNameProvider='avvDNS' + $this.ToTitleCase($this.provider)
+        $result.compute.add('Method', $Method)
+        $result.compute.add('ClassNameProvider', $ClassNameProvider)
+        try
         {
-            $result.status=1 # нет такого провайдера
-            $result.result=@()
+            if ($this.provider.ToLower() -ne '_none_')
+            {
+                # Подключение файла реализации класса
+                try
+                {
+                    $NameModule = "$( $this.pwd )\$( $ClassNameProvider ).ps1"
+                    $result.compute.add('NameModule', $NameModule)
+                    #Import-Module ".\$($classNameProvider).ps1"
+                    #Import-Module $NameModule
+                    . "$( $this.pwd )\avvDNSProvider.ps1"
+                    . $NameModule
+                }
+                catch
+                {
+                    # "Ошибка открытия файла реализации API {0} для класса {1}"
+                    $result.status = 2
+                    $arrObj = @($NameModule, $ClassNameProvider)
+                    $result.msgException = $PSItem.Exception.Message
+                    throw $PSItem
+                }
+
+                # Создание экземпляра класса реализации
+                try
+                {
+                    #$classProvider=New-Object -TypeName $classNameProvider -ArgumentList $this.provider;
+                    #$ClassProvider=New-Object -TypeName $ClassNameProvider -ArgumentList $this.params;
+                    # $ClassNameProvider+="1" # TEST-EXCEPTION
+                    $ClassProvider = New-Object -TypeName $ClassNameProvider;
+                    $result.compute.add('classProvider', $ClassProvider.Methods)
+                }
+                catch
+                {
+                    # "Ошибка создания экземпляра класса {0}"
+                    $result.status = 4
+                    $arrObj = @($ClassNameProvider)
+                    $result.msgException = $PSItem.Exception.Message
+                    throw $PSItem
+                }
+
+                # проверить существование описания переданного $Method в классе реализации
+                try
+                {
+                    #$Method += '1' # TEST-EXCEPTION
+                    $ComPart1="MethodIsImplemented('$( $Method )')"
+                    #$ComPart1="MethodIsImplemented1('$( $Method )')" # TEST_ECEPTION
+                    #$CommandIsImplemented = '$ClassProvider.' + "MethodIsImplemented('$( $Method )')"
+                    $CommandIsImplemented = '$ClassProvider.' + $ComPart1
+                    $result.compute.add('CommandIsImplemented', $CommandIsImplemented)
+                    $MethodIsImplemented=Invoke-Expression -Command ($CommandIsImplemented)
+                    #$MethodIsImplemented = Invoke-Expression -Command ('$classProvider.' + "MethodIsSupported('$( $Method )')")
+                    $result.compute.add('MethodIsImplemented', $MethodIsImplemented)
+                }
+                catch
+                {
+                    # "Ошибка вызова метода {0} класса {1}"
+                    $result.status = 5 # Метод
+                    $arrObj = @(
+                        '[' + "$ClassNameProvider" + '].' + $ComPart1
+                        $ClassNameProvider
+                    )
+                    $result.msgException = $PSItem.Exception.Message
+                    throw $PSItem
+                }
+
+                # Подготовить к вызову функцию для $Method
+                try
+                {
+                    if ($MethodIsImplemented)
+                    {
+                        # Если существует функция в классе для $method
+                        try
+                        {
+                            # вернуть функцию-член класса реализующую функционал метода
+                            #$Method += '1' # TEST-EXCEPTION
+                            $ComPart1="GetImplementationMethod('$( $Method )')"
+                            #$ComPart1="MethodIsImplemented1('$( $Method )')" # TEST_ECEPTION
+                            $ImplMethod = Invoke-Expression -Command ('$classProvider.' + $ComPart1)
+                            $result.compute.add('ImplMethod', $ImplMethod)
+                        }
+                        catch
+                        {
+                            # "Ошибка вызова метода {0} класса {1}"
+                            $result.status = 5 # Метод
+                            $arrObj = @(
+                            '[' + "$ClassNameProvider" + '].' + $ComPart1
+                            $ClassNameProvider
+                            )
+                            $result.msgException = $PSItem.Exception.Message
+                            throw $PSItem
+                        }
+                    }
+                    else
+                    {
+                        # Если не существует функции в классе реализации для $Method
+                        # "Метод {0} не поддерживается в данном классе реализации {1}"
+                        $result.status = 6
+                        $arrObj=@($Method, $ClassNameProvider)
+                        $result.result = @()
+                        throw [System.InvalidOperationException]::New($errors[$result.status] -f $arrObj)
+                        #$result.msgException = "Нет реализации провайдера $($this.provider)"
+                    }
+
+                    #$res=$classProvider.MethodDispath($Method, $this.params)
+                }
+                catch
+                {
+                    $result.msgException = $PSItem.Exception.Message
+                    throw $PSItem
+                }
+
+                # Вызов функции-реализации
+                try
+                {
+                    # вызвать функцию-член класса реализующую функционал метода
+                    $ImplMethod = '' # TEST-EXCEPTION
+                    if ($ImplMethod)
+                    {
+                        # если существует реализация
+                        #echo "fsd"
+                    }
+                    else
+                    {
+                        # не существует реализация
+                        # "Метод {0} не поддерживается в данном классе реализации {1}"
+                        $result.status = 6
+                        $arrObj=@($Method, $ClassNameProvider)
+                        $result.result = @()
+                        throw [System.InvalidOperationException]::New($errors[$result.status] -f $arrObj)
+                        #$result.msgException = "Нет реализации провайдера $($this.provider)"
+
+                    }
+                }
+                catch
+                {
+                    #"Нет провайдера {0}"
+                    #"Ошибка открытия файла реализации API {0} для класса {1}"
+                    #"Нет класса {0}"
+                    $result.status = 2 # нет такого класса, файла с классом, ошибка создания объекта
+                    $result.msgException = $PSItem.Exception.Message
+                    throw $PSItem
+                }
+            }
+            else
+            {
+                # "Нет реализации API для провайдера {0}"
+                $result.status = 1
+                $arrObj=@($this.provider)
+                $result.result = @()
+                #$result.msgException = "Нет реализации провайдера $($this.provider)"
+            }
         }
-        $arrObj=@($this.provider, $classNameProvider)
-        $result.msg=$errros[$result.status] -f $arrObj
+        catch {}
+
+        #$arrObj=@($this.provider, $classNameProvider, $NameModule)
+        $result.msg=$errors[$result.status] -f $arrObj
         return $result
     }
 
@@ -104,7 +252,7 @@ class avvDNSBase{
         }
         else
         {
-            $ar=$value.Split(" ").ForEach({if ($_.Length) {$_.Substring(0,1).ToUpper()+$_.Substring(1,($_.Length-1))} else {$_}})
+            $ar=$value.Split(" ").ForEach({if ($_.Length) {$_.Substring(0,1).ToUpper()+$_.Substring(1,($_.Length-1)).ToLower()} else {$_}})
             $result=[string]::Join(" ", $ar)
         }
         return $result
--- classes/avvDNSSelectel.ps1
+++ classes/avvDNSSelectel.ps1
@@ -1,5 +1,21 @@
-class avvDNSSelectel
-{
-    $test='wqe'
+using module '.\avvDNSProvider.ps1'
 
+#using module "D:\tools\selectel.dns-hosting\classes\avvDNSSelectel.ps1"
+#. .\avvDNSProvider.ps1
+
+class avvDNSSelectel : avvDNSProvider
+{
+
+    avvDNSSelectel() : base() {
+        $this.Methods.Add('GetDomains', '')
+    }
+
+    [Hashtable] GetDomains([Hashtable]$Arguments)
+    {
+        $res=@{}
+        $res.add('Provider', 'Selectel')
+        return $res
+    }
+
+    #avvDNSSelectel
 }
