a6771da4ae818894ed233fcf82ef425497941861
vovka <9141778236@mail.ru>
2022-07-01 21:48:00 +1000

    3.1.0 Релиз закрыл. Будем менять класс Provider в release/4

--- classes/avvDNSClinic.ps1
+++ classes/avvDNSClinic.ps1
@@ -3,10 +3,11 @@ using module '.\avvDNSProvider.ps1'
 #using module "D:\tools\selectel.dns-hosting\classes\avvDNSSelectel.ps1"
 #. .\avvDNSProvider.ps1
 
-class avvDNSMydns : avvDNSProvider
+class avvDNSClinic : avvDNSProvider
 {
     [Hashtable]hidden $Methods=@{
-        'gd'='GetDomains'
+        'help'='GetHelp'
+        'version'='GetVersion'
         'gr'='GetRecords'
     }
 
@@ -42,9 +43,30 @@ class avvDNSMydns : avvDNSProvider
         return $res
     }
 
+    static [Hashtable] GetHelp([Hashtable]$Arguments)
+    {
+        #GET https://api.t.mrovo.ru/api/hs/er/v1/info
+        $res=[avvDNSClinic]::Request(@{
+            'Method'='Get'
+            'Uri'="https://dbg.alt.av-kms.ru/api_dng/hs/er/v1/help"
+        })
+        return $res
+    }
+
+    static [Hashtable] GetVersion([Hashtable]$Arguments)
+    {
+        #GET https://api.t.mrovo.ru/api/hs/er/v1/info
+        $res=[avvDNSClinic]::Request(@{
+            'Method'='Post'
+            'Uri'="https://dbg.alt.av-kms.ru/api_dbg/hs/er/v2/version"
+            'Body'="{'ext':'1'}"
+        })
+        return $res
+    }
+
     static [Hashtable] GetDomains([Hashtable]$Arguments)
     {
-        $res=[avvDNSMydns]::Request(@{
+        $res=[avvDNSClinic]::Request(@{
             'Method'='Get'
             'Headers'=@{"X-Token"="$($Arguments.token)";"Content-Type"="application/json"}
             'Uri'="https://api.selectel.ru/domains/v1/"
@@ -54,7 +76,7 @@ class avvDNSMydns : avvDNSProvider
 
     static [Hashtable] GetRecords([Hashtable]$Arguments)
     {
-        $res=[avvDNSMydns]::Request(@{
+        $res=[avvDNSClinic]::Request(@{
             'Method'='Get'
             'Headers'=@{"X-Token"="$($Arguments.token)";"Content-Type"="application/json"}
             'Uri'="https://api.selectel.ru/domains/v1/$( $Arguments.domain )/records"
