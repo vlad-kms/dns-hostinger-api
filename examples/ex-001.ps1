e801055b33ee3263ff9ab8d96c83a99bba02fd99
Vladimir Alexeev <9141778236@mail.ru>
2022-07-20 18:13:53 +1000

    Изменил examples ex-001, добавил ini-файл

--- examples/ex-001.ps1
+++ examples/ex-001.ps1
@@ -7,3 +7,6 @@ $q2.result;$q2.response
 
 $global:q3=.\dns-cli.ps1 -Debug -Action 'doctors' -ExtParams @{'ExtUri'='370f01fe-2f39-11e9-bec0-f44d30eed1e5/workplaces'; 'erewr'='cvcx'} -Provider 'clinic';
 $q3.result;$q3.response
+
+$global:q4=.\dns-cli.ps1 -Debug -Action 'doctors' -ExtParams @{'ExtUri'='370f01fe-2f39-11e9-bec0-f44d30eed1e5/workplaces'; 'access_token'='access_token'} -Provider 'clinic';
+$q4.result;$q4.response
